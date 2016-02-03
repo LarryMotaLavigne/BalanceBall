31ed28be5ff5e76dd414fb227a0583471d7bfcf2
Squalex <larry.motalavigne@gmail.com>
2016-02-03 14:28:12 +0100

    Handle ball speed accelerometer

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,5 +15,22 @@ public class CameraController : MonoBehaviour {
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+        OrbitAround();
+    }
+
+    void OrbitAround()
+    {
+        Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+        Vector2 firstVector = lastFramePos - screenCenter;
+        Vector2 convertedMouseInput = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 secondVector = convertedMouseInput - screenCenter;
+        float angle = Vector2.Angle(firstVector, secondVector);
+        Vector3 cross = Vector3.Cross(firstVector, secondVector);
+        if (cross.z < 0)
+        {
+            angle = -angle;
+        }
+        var rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + angle, 0);
+        transform.rotation = rotation;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
+    public Text text;
 
     private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
+
+    // The initials orientation
+    private int initialOrientationX;
+    private int initialOrientationY;
+    private int initialOrientationZ;
+
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        /*
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        */
-        float moveHorizontal = Input.acceleration.x;
-        float moveVertical = -Input.acceleration.z;
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        float moveVertical;
+        float moveHorizontal;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            moveHorizontal = 3*Input.acceleration.x;
+            moveVertical = 3*Mathf.Clamp(-Input.acceleration.z - 0.6f,-1.0f,1.0f);
+        }
+        else
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            moveVertical = Input.GetAxis("Vertical");
+        }
 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        text.text = "(Horizontal :"+moveHorizontal+", Vertical : "+moveVertical+")";
         rb.AddForce(movement * speed);
     }
 

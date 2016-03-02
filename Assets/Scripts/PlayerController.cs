a3e8d4e217d7fd53e1383e96aef1b627291fccf2
Larry <larry.motalavigne@gmail.com>
2016-03-03 00:04:33 +0100

    - Fonctionnalités billes (sous PC) - Chargement des compétences - Animation de mort (pas finis) - Menu reformaté - Bouton d'option et de sortie sur le menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerController : MonoBehaviour {
 
@@ -13,24 +15,36 @@ public class PlayerController : MonoBehaviour {
 
     /***** ENVIRONMENT VARIABLES *****/
 
-    public float speed;
-    public float jumpSpeed;
+    public float speed = 10;
+    public float jumpSpeed = 500;
+    public float dashSpeed = 500;
     public float gravity;
     public Text text;
     public bool grounded = true;
     public bool canDoubleJump = isDoubleJumpUnlocked;
+
+
+    // 3 health points
+    public RawImage health1;
+    public RawImage health2;
+    public RawImage health3;
+
+
+    // Animator to control the death Animation
+    public Animator deathAnim;
+
 
     /***** ORIENTATION and BODY *****/
     private Rigidbody rb;
     private Vector3 originPosition = Vector3.zero;
     private Vector3 moveDirection = Vector3.zero;
 
-    // Use this for initialization
-    void Start () {
+
+    void Start ()
+    {
         rb = GetComponent<Rigidbody>();
     }
 	
-	// Update is called once per frame
 	void FixedUpdate () {
         
         if (Application.platform == RuntimePlatform.Android)
@@ -46,13 +60,17 @@ public class PlayerController : MonoBehaviour {
 
         jump();
         dash();
+        shrink();
         death();
+
 
         //text.text = "(Horizontal :"+moveDirection.x+", Vertical : "+moveDirection.z+")";
         text.text = "Position " + rb.position.y;
         rb.AddForce(moveDirection * speed);
     }
 
+
+    // Handle the collision
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
@@ -61,6 +79,11 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+
+
+    /*****************************************************************/
+    /**********               JUMP & DOUBLE JUMP            **********/
+    /*****************************************************************/
     void jump()
     {
         if(Input.GetMouseButtonDown(0)) // Touch on the screen or Right Click
@@ -90,28 +113,71 @@ public class PlayerController : MonoBehaviour {
         }
 
     }
+    
 
-
+    /*****************************************************************/
+    /**********                     DASH                    **********/
+    /*****************************************************************/
     void dash()
     {
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(1) && CircularLoadingDash.dashAvailable)
         {
-            rb.AddForce(new Vector3(0.0f,0.0f, 1000.0f));
+            rb.AddForce(new Vector3(moveDirection.x * dashSpeed, 0.0f, moveDirection.z * dashSpeed));
+            CircularLoadingDash.resetTimer = true;
         }
     }
 
+    
+
+    /*****************************************************************/
+    /**********                    SHRINK                   **********/
+    /*****************************************************************/
     void shrink()
     {
-
+        if(Input.GetMouseButtonDown(2) && CircularLoadingShrink.shrinkAvailable)
+        {
+            if(rb.transform.localScale.x.Equals(1.0f))
+            {
+                rb.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            }
+            else
+            {
+                rb.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            }
+            CircularLoadingShrink.resetTimer = true;
+        }
     }
 
+    
 
+    /*****************************************************************/
+    /**********                    DEATH                    **********/
+    /*****************************************************************/
     void death()
     {
         if(rb.position.y < -5)
         {
-            text.text = "You're dead to me !";
+            if(health3.isActiveAndEnabled)
+            {
+                health3.enabled = false;
+            }
+            else if(health2.isActiveAndEnabled)
+            {
+                health2.enabled = false;
+            }
+            else if(health1.isActiveAndEnabled)
+            {
+                health1.enabled = false;
+                deathAnim.SetTrigger("GameOver"); // launch animation 
+                
+                // Problème ici d'attente avant de charger la nouvelle scène
+                // Solution 1.  Une coroutine avec un wait
+                // Solution 2. Interface... Il doit y avoir un moyen de faire en sorte que l'animation soit ininterruptible
+
+                SceneManager.LoadScene("menu");
+            }
             rb.MovePosition(originPosition);
         }
     }
+
 }
